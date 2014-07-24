31b0c188b38645fbd08f936af076e99665f611fd
unknown <573807@BAHCNU4069ZZJ.resource.ds.bah.com>
2014-07-24 11:15:07 -0400

    code not working, seeking help

---
+++ src/com/example/helloworld/DeviceControlActivity.java
@@ -0,0 +1,71 @@
+package com.example.helloworld;
+
+import android.app.Activity;
+import android.content.ComponentName;
+import android.content.Intent;
+import android.content.ServiceConnection;
+import android.os.Bundle;
+import android.os.IBinder;
+import android.util.Log;
+import android.view.KeyEvent;
+
+
+public class DeviceControlActivity extends Activity {
+    private final static String TAG = "BETH"; //DeviceControlActivity.class.getSimpleName();
+ 
+    public static final String EXTRAS_DEVICE_NAME = "DEVICE_NAME";
+    public static final String EXTRAS_DEVICE_ADDRESS = "DEVICE_ADDRESS";
+ 
+    private String mDeviceName;
+    private String mDeviceAddress;
+    private BluetoothLeService mBluetoothLeService;
+ 
+    
+    // Code to manage Service lifecycle.
+    private final ServiceConnection mServiceConnection = new ServiceConnection() {
+ 
+        @Override
+        public void onServiceConnected(ComponentName componentName, IBinder service) {
+            mBluetoothLeService = ((BluetoothLeService.LocalBinder) service).getService();
+            if (!mBluetoothLeService.initialize()) {
+                Log.e(TAG, "Unable to initialize Bluetooth");
+                finish();
+            }
+            // Automatically connects to the device upon successful start-up initialization.
+            mBluetoothLeService.connect(mDeviceAddress);
+        }
+ 
+        @Override
+        public void onServiceDisconnected(ComponentName componentName) {
+            mBluetoothLeService = null;
+        }
+    };
+ 
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        
+        final Intent intent = getIntent();
+        mDeviceName = intent.getStringExtra(EXTRAS_DEVICE_NAME);
+        mDeviceAddress = intent.getStringExtra(EXTRAS_DEVICE_ADDRESS);
+        
+        Intent gattServiceIntent = new Intent(this, BluetoothLeService.class);
+        bindService(gattServiceIntent, mServiceConnection, BIND_AUTO_CREATE);
+        
+    }
+
+    /*
+     * When you touch the glass keypad, start listening for magnetometer data
+     */
+	@Override
+    public boolean onKeyDown(int keycode, KeyEvent event) {
+        mBluetoothLeService.turnOnMagnetometer();
+        mBluetoothLeService.enableMagnetometerNotifications();
+       
+        return false;
+         
+    }
+	
+
+	
+}
---
+++ src/com/example/helloworld/MainActivity.java
@@ -0,0 +1,52 @@
+package com.example.helloworld;
+
+
+import android.app.Activity;
+import android.bluetooth.BluetoothAdapter;
+import android.bluetooth.BluetoothDevice;
+
+import android.bluetooth.BluetoothManager;
+
+import android.content.Context;
+import android.content.Intent;
+import android.os.Bundle;
+
+public class MainActivity extends Activity {
+	
+	private BluetoothAdapter mBluetoothAdapter;
+  
+	 
+	private BluetoothAdapter.LeScanCallback mLeScanCallback = new BluetoothAdapter.LeScanCallback() {
+	    @Override
+	    public void onLeScan(final BluetoothDevice device, int rssi,
+	            byte[] scanRecord) {
+	        runOnUiThread(new Runnable() {
+	           @Override
+	           public void run() {
+	        	  
+	        	   mBluetoothAdapter.stopLeScan(mLeScanCallback);
+
+	        	   final Intent intent = new Intent(getApplicationContext(), DeviceControlActivity.class);
+	               intent.putExtra(DeviceControlActivity.EXTRAS_DEVICE_NAME, device.getName());
+	               intent.putExtra(DeviceControlActivity.EXTRAS_DEVICE_ADDRESS, device.getAddress());
+	            
+	               startActivity(intent);
+
+	           }
+	       });
+	   }
+
+	};
+	
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {	
+        super.onCreate(savedInstanceState);
+
+        final BluetoothManager bluetoothManager = (BluetoothManager) getSystemService(Context.BLUETOOTH_SERVICE);
+        mBluetoothAdapter = bluetoothManager.getAdapter();
+        mBluetoothAdapter.startLeScan(mLeScanCallback);
+         
+    }
+    
+
+}
